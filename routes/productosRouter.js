57e1d420a5c872ae95b9d5562155bb2e9632901f
TmsLeOnGithub <tomasleon2012@gmail.com>
2022-10-11 20:32:35 -0300

    DESAFIO 4

---
+++ routes/productosRouter.js
@@ -0,0 +1,43 @@
+import express from "express";
+import { Contenedor } from '../containers/contenedor.js';
+
+const contenedor = new Contenedor('productos.txt');
+const app = express();
+const PORT = 8080;
+const { Router } = express;
+export const productosRouter = Router();
+
+productosRouter.get('/', (req, res) => {
+    contenedor.getAll().then(productos => {
+        res.send(productos);
+        res.end();
+    });
+})
+
+productosRouter.get('/:id', (req, res) => {
+    contenedor.getById(Number(req.params?.id)).then(producto => {
+        res.send(producto ? producto : {error: 'Producto no encontrado'});
+        res.end();
+    })
+})
+
+productosRouter.post('/', (req, res) => {
+    console.log(req.body);
+    const producto = req.body;
+    contenedor.save(producto).then((item) => {
+        res.status(200).send(item)
+    })
+})
+
+productosRouter.put('/:id', (req, res) => {
+    contenedor.update(Number(req.params?.id), req.body).then((item) => {
+        res.status(200).send(item)
+    })
+})
+
+productosRouter.delete('/:id', (req, res) => {
+    contenedor.deleteById(Number(req.params?.id)).then(() => {
+        res.status(200).send();
+        res.end();
+    })
+});
